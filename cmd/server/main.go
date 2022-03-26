4c163f51d507b60ba8c77c2128117e38cdac9e79
lil-newty <igor.holanda.main@gmial.com>
2022-03-26 18:59:27 -0300

    Added health check endpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,14 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	transportHTTP "github.com/lil-newty/go-rest-api-01/internal/transport/http"
+)
 
 // App - the struct which contains thinks like
 // pointers to database connections
 type App struct{}
 
 // Run - sets up our application
-funct (app *App) Run() error {
-	fmt.Println("Setting up our app")
+func (app *App) Run() error {
+	fmt.Println("Setting Up Our App")
+
+	handler := transportHTTP.NewHandler()
+	handler.SetupRoutes()
+
+	if err := http.ListenAndServe(":9090", handler.Router); err != nil {
+		fmt.Println("Failed to set up server")
+		return err
+	}
+
 	return nil
 }
 
@@ -16,7 +30,7 @@ func main() {
 	fmt.Println("Go REST API ")
 	app := App{}
 	if err := app.Run(); err != nil {
-		fmt.Println("Error starting up our REST API")
+		fmt.Println("Error Starting Up Our REST API")
 		fmt.Println(err)
 	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/lil-newty/go-rest-api-01
 
 go 1.17
+
+require github.com/gorilla/mux v1.8.0 // indirect
